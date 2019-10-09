4475b33a9cbe8808113729b99dbfc04106460e6e
fwalocha <fawalocha@gmail.com>
2019-10-09 15:41:29 +0200

    Finish modules for GA v0.1

--- walocha_dijkstra_GA.R
+++ walocha_dijkstra_GA.R
@@ -10,6 +10,7 @@ function walocha_dijkstra_GA(dimension, obj_func,
 							 target = NULL, lambda_ = 100, mu_ = 15,  
 							 budget = NULL, set_parameters = NULL) {
 
+
 	parents = initialize()
 
 	while(budget>0) {
@@ -43,6 +44,13 @@ function walocha_dijkstra_GA(dimension, obj_func,
 }
 
 function genetic_algorithm(IOHproblem){
+
+
+  target_hit <- function(f) {
+    return(any(f>=IOHproblem$fopt))
+  }
+
+
   mu = ...;
   pc = ...;
   pm = ...;
@@ -64,16 +72,16 @@ function genetic_algorithm(IOHproblem){
   # Evolution loop
   
   Pnew = rep.int(NA, mu);
-  while(evalcount<budget && IOHproblem$hit_optimal(problem)){
+  while(evalcount<budget && !target_hit(f)){
     for(i in 1:mu){
-      p1 = selection(P);
-      if(unif(1) < pc) {
-        p2 = selection(P);
-		    Pnew[i,] = crossover(p1,p2);
+      p1 = selection(f, NA);
+      if(runif(1) < pc) {
+        p2 = selection(f, p1);
+		    Pnew[i,] = crossover(P[p1,],P[p2,]);
       } else {
-        Pnew[i,] = copy();
+        Pnew[i,] = P[p1];
       }
-      Pnew[i,] = mutate(Pnew[i,]);
+      Pnew[i,] = mutate(Pnew[i,], pm);
     }
     P = Pnew;
     
@@ -87,8 +95,51 @@ function genetic_algorithm(IOHproblem){
 }
 
 
-# TODO: what does selection look like? 
-# TODO: How to decode genome?
+
+function selection(f, index){
+  if(!is.na(index)){
+    f = f[-index,];
+  }
+  old_idxs = 1:length(f);
+  new_idxs = argsort(f, decreasing=TRUE);
+  old_idxs = old_idxs[new_idxs];
+  f = f[new_idxs];
+  f = f/sum(f);
+
+  winner = which(f>runif(1))[1];
+  return(f[old_idxs[winner]]);
+}
+
+
+crossover <- function(p1, p2){
+  n = length(p1)
+  intersect = sample(1:n-1,1);
+  return(c(p1[1:intersect],p2[intersect+1:n]))
+}
+
+mutation <- function(p,pm){
+  return(sapply(p,function(x){
+    if(runif(1)<pm){
+      return(1-x);
+    } else{
+      return(x);
+    }
+  })
+  )
+}
+
+
+decode <- function(P){
+  return(P);
+}
+
+evaluate <- function(G, IOHproblem) {
+  return(IOHproblem$obj_func(G));  
+}
+
+
+# TODO: what does selection look like? OK
+# TODO: How to decode genome? OK
 # TODO: How to do evaluation?
 
 
