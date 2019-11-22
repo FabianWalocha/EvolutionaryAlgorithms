a255892a3b5fe758b29fb5d80050d04725943dbe
fwalocha <fawalocha@gmail.com>
2019-11-22 18:08:05 +0100

    Final version 1.0 classic approach ES

--- walocha_dijkstra_ES.R
+++ walocha_dijkstra_ES.R
@@ -8,7 +8,7 @@ devtools::install_github('IOHprofiler/IOHexperimenter@R-restructure')
 library('IOHexperimenter')
 
 mu = 15; #number of parents
-rho = 15;
+rho = 10;
 lambda = 100; #size of offspring
 sigma <<- 0;
 
@@ -19,7 +19,7 @@ evolutionary_strategy <- function(IOHproblem){
   # Define functions
   
   target_hit <- function(f, IOHproblem) { # is the target value found, returns bool?
-    return(any(f<=IOHproblem$fopt));
+    return(IOHproblem$target_hit());
   }
   
   evaluate <- function(P, IOHproblem){ # returns the value of the objective function for a population
@@ -28,43 +28,50 @@ evolutionary_strategy <- function(IOHproblem){
   }
   
   select <- function(f, mu){ 
-    return(order(f, decreasing = TRUE)[seq(mu)]); #return indices of best
+    return(order(f)[seq(mu)]); #return indices of best
   }
-  
-  recombine <- function(P, f, lambda, rho, discrete=FALSE){ ## add local
-    if(discrete){ # each feature is a copy of one of the parents
-      if(rho==dim(P)[1]){
-        Psel = P;
-      } else {
-        rho_best = order(f, decreasing=TRUE) # I dont think it should be ordered, but random selection?
-        Psel = t(P[rho_best])
+    
+  recombine <- function(P, f, lambda, rho, discrete=FALSE, local=FALSE){ ## add local
+    mu = dim(P)[1];
+    offsprings = matrix(seq(lambda*dim(P)[2]), nrow=lambda)
+    if(rho==mu){
+      Psel = P;
+      if(discrete){ # each feature is a copy of one of the parents
+        for(idx in seq(lambda)){
+          offsprings[idx,] = apply(Psel, 2, function(x) x[sample(rho,1)]) #returns recombined offspring
+        }
+        return(offsprings)
+      } else{ # take average of the parents
+        offspring = colMeans(Psel); # take the average of all parents for each feature
+        return(matrix(rep(offspring,lambda),nrow=lambda)); #does this make sense? returning same off spring 100 times incase of not discrete?
       }
-      offspring = apply(Psel, 2, function(x) x[sample(1:rho)]);
-    } else{ # take average of the parents
-      if(rho == dim(P)[1]){
-        Psel = P;
-        
-      } else {
-        rho_best = order(f, decreasing=TRUE);
-        Psel = P[rho_best];
+    } else {
+      rho_rand = matrix(seq(rho*lambda), nrow= lambda)
+      rho_rand = t(apply(rho_rand,1,function(x) sample(mu,rho)))
+      # rho_best = order(f)[seq(rho)]; # I dont think it should be ordered, but random selection?
+      # Psel = P[rho_best,];
+      for(idx in seq(lambda)){
+        Psel = P[rho_rand[idx,],]
+        if(discrete){ # each feature is a copy of one of the parents
+          offsprings[idx,] = apply(Psel, 2, function(x) x[sample(rho,1)])
+        } else{ # take average of the parents
+          offsprings[idx,] = colMeans(Psel); # take the average of all parents for each feature
+        }
       }
-      offspring = colMeans(Psel); # take the average of all parents for each feature
+      return(offsprings)
     }
-    return(matrix(rep(offspring,lambda),nrow=lambda)); #does this make sense? returning same off spring 100 times incase of not discrete?
   }
   
   mutate <- function(P, tau, use_corr=FALSE,adapt=FALSE){
     if(length(sigma)>1){ # change boolean to global and local instead?
       rv_global = rnorm(1)
       assign("sigma", sigma * exp(tau[1]*rv_global+tau[2]*rnorm(length(sigma))), envir=.GlobalEnv); # update all sigma's
-      P = apply(P,1, function(x) x+sigma*rnorm(length(sigma))) # do mutation with stepsize sigma # can go out if statement?
+      P = apply(P,2, function(x) x+sigma*rnorm(length(sigma))) # do mutation with stepsize sigma # can go out if statement?
     } else {
-      print(sigma)
-      assign("sigma", sigma*exp(tau*rnorm(1))); # update only the global sigma
-      print(sigma)
-      P = apply(P,1, function(x) x+sigma*rnorm(length(sigma))) # do mutation with stepsize sigma
+      assign("sigma", sigma*exp(tau*rnorm(1)), envir=.GlobalEnv); # update only the global sigma
+      P = apply(P,2, function(x) x+sigma*rnorm(length(sigma))) # do mutation with stepsize sigma
     }
-    P = apply(P, c(1,2), function(x) min(max(P,-5),5)) # bound the possible values between -5 and 5
+    P = apply(P, c(1,2), function(x) min(max(x,-5),5)) # bound the possible values between -5 and 5
     return(P)  
   }
   
@@ -72,10 +79,10 @@ evolutionary_strategy <- function(IOHproblem){
   
   # Initialization
   
-  budget = 10000;
   evalcount <<- 0;
   n = IOHproblem$dimension; # input dimensionality of length n. 
-  fopt = -Inf; # optimal value of objective function
+  budget = 10000*n;
+  fopt = Inf; # optimal value of objective function
   
   xopt = matrix(data=NA, nrow=mu, ncol=1); # find optimal matrix that gives fopt
   P = matrix(runif(mu*n, -5,5),nrow = mu) # Population of size m with dimension (features) n
@@ -84,6 +91,7 @@ evolutionary_strategy <- function(IOHproblem){
   
   # sigma is the mutation step size coevolving with x
   sigmaValue <- mean(abs(f-IOHproblem$fopt)/sqrt(n))
+  # sigmaValue <- abs(f-IOHproblem$fopt)/sqrt(n);
   assign("sigma", sigmaValue, envir=.GlobalEnv); #update the sigma globally
   
   # tau is the learning rate
@@ -98,27 +106,28 @@ evolutionary_strategy <- function(IOHproblem){
   # Evolution loop
   
   while(evalcount<=(budget-lambda) && !target_hit(f, IOHproblem)){
-    Prec = recombine(P, f, lambda, rho);
+    Prec = recombine(P, f, lambda, rho, discrete = TRUE);
     Pmut = mutate(Prec, tau);
     f = evaluate(Pmut,IOHproblem); # mu komma lambda selection?
     idxs = select(f,  mu);
     Pnew = Pmut[idxs];
     f = f[idxs];
-    if(any(f>fopt)){
-      fopt = max(f);
-      xopt = P[order(f,decreasing = TRUE)[1]];
+    if(any(f<fopt)){
+      fopt = min(f);
+      xopt = P[order(f)[1],];
     }
   }
+  print(paste(min(c(fopt,f))-IOHproblem$fopt, evalcount))
 }
 
 
 benchmark_algorithm(user_alg=evolutionary_strategy,
                     instances=seq(5),
-                    dimensions=c(2,5,20), 
+                    dimensions=c(2), 
                     functions=seq(24),
                     repetitions = 5,
                     suite="BBOB",
                     data.dir='./data/es/', 
                     params.track = 'sigma',
-                    algorithm.name = paste('ES-(',mu,',',lambda,'),', sep = ""), 
-                    algorithm.info = paste('(',mu,',',lambda,') evolutionary strategy',sep = ""))
+                    algorithm.name = paste('ES-(',mu,'-',lambda,')', sep = ""), 
+                    algorithm.info = paste('(',mu,'-',lambda,') evolutionary strategy',sep = ""))
