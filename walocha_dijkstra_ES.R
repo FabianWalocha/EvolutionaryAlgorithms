a2da13bd63cc4fda32256cddd7d64e53226f8c1e
ElinBD <31306311+ElinBD@users.noreply.github.com>
2019-11-22 13:42:18 +0100

    added comments by Elin

--- walocha_dijkstra_ES.R
+++ walocha_dijkstra_ES.R
@@ -14,28 +14,33 @@ sigma <<- 0;
 
 evolutionary_strategy <- function(IOHproblem){
   
-  target_hit <- function(f, IOHproblem) {
+  ################################################
+  
+  # Define functions
+  
+  target_hit <- function(f, IOHproblem) { # is the target value found, returns bool?
     return(any(f<=IOHproblem$fopt));
   }
-  evaluate <- function(P, IOHproblem){
+  
+  evaluate <- function(P, IOHproblem){ # returns the value of the objective function for a population
     assign("evalcount", evalcount+dim(P)[1], envir=.GlobalEnv);
-    return(IOHproblem$obj_func(P));  # return the value of the objective function
+    return(IOHproblem$obj_func(P));  
   }
   
   select <- function(f, mu){ 
     return(order(f, decreasing = TRUE)[seq(mu)]); #return indices of best
   }
   
-  recombine <- function(P, f, lambda, rho, discrete=FALSE){
-    if(discrete){
+  recombine <- function(P, f, lambda, rho, discrete=FALSE){ ## add local
+    if(discrete){ # each feature is a copy of one of the parents
       if(rho==dim(P)[1]){
         Psel = P;
       } else {
-        rho_best = order(f, decreasing=TRUE)
+        rho_best = order(f, decreasing=TRUE) # I dont think it should be ordered, but random selection?
         Psel = t(P[rho_best])
       }
       offspring = apply(Psel, 2, function(x) x[sample(1:rho)]);
-    } else{
+    } else{ # take average of the parents
       if(rho == dim(P)[1]){
         Psel = P;
         
@@ -43,53 +48,59 @@ evolutionary_strategy <- function(IOHproblem){
         rho_best = order(f, decreasing=TRUE);
         Psel = P[rho_best];
       }
-      offspring = colMeans(Psel);
+      offspring = colMeans(Psel); # take the average of all parents for each feature
     }
-    return(matrix(rep(offspring,lambda),nrow=lambda));
+    return(matrix(rep(offspring,lambda),nrow=lambda)); #does this make sense? returning same off spring 100 times incase of not discrete?
   }
   
   mutate <- function(P, tau, use_corr=FALSE,adapt=FALSE){
-    if(length(sigma)>1){
+    if(length(sigma)>1){ # change boolean to global and local instead?
       rv_global = rnorm(1)
-      assign("sigma", sigma * exp(tau[1]*rv_global+tau[2]*rnorm(length(sigma))), envir=.GlobalEnv);
-      P = apply(P,1, function(x) x+sigma*rnorm(length(sigma)))
+      assign("sigma", sigma * exp(tau[1]*rv_global+tau[2]*rnorm(length(sigma))), envir=.GlobalEnv); # update all sigma's
+      P = apply(P,1, function(x) x+sigma*rnorm(length(sigma))) # do mutation with stepsize sigma # can go out if statement?
     } else {
       print(sigma)
-      assign("sigma", sigma*exp(tau*rnorm(1)));
+      assign("sigma", sigma*exp(tau*rnorm(1))); # update only the global sigma
       print(sigma)
-      P = apply(P,1, function(x) x+sigma*rnorm(length(sigma)))
+      P = apply(P,1, function(x) x+sigma*rnorm(length(sigma))) # do mutation with stepsize sigma
     }
-    P = apply(P, c(1,2), function(x) min(max(P,-5),5))
+    P = apply(P, c(1,2), function(x) min(max(P,-5),5)) # bound the possible values between -5 and 5
     return(P)  
   }
   
-  ###################################################### Check what IOHproblem$obj_func is
+  ###################################################### 
+  
+  # Initialization
   
   budget = 10000;
-  n = IOHproblem$dimension; # input dimensionality of length n. 
-  fopt = -Inf;
-  
-  
-  xopt = matrix(data=NA, nrow=mu, ncol=1);
-  
   evalcount <<- 0;
+  n = IOHproblem$dimension; # input dimensionality of length n. 
+  fopt = -Inf; # optimal value of objective function
   
-  # initialization
-  P = matrix(runif(mu*n, -5,5),nrow = mu)
-  f = evaluate(P,IOHproblem);
-  assign("sigma", mean(abs(f-IOHproblem$fopt)/sqrt(n)), envir=.GlobalEnv);
-  if(length(sigma)>1){
-    tau = c(1/sqrt(2*n), 1/sqrt(2*sqrt(n)));
-  } else {
-    tau = 1/sqrt(n);
+  xopt = matrix(data=NA, nrow=mu, ncol=1); # find optimal matrix that gives fopt
+  P = matrix(runif(mu*n, -5,5),nrow = mu) # Population of size m with dimension (features) n
+  Pnew = matrix(data=NA, nrow=mu, ncol=n); # Offspring/children population
+  f = evaluate(P,IOHproblem); # fitness values of the population
+  
+  # sigma is the mutation step size coevolving with x
+  sigmaValue <- mean(abs(f-IOHproblem$fopt)/sqrt(n))
+  assign("sigma", sigmaValue, envir=.GlobalEnv); #update the sigma globally
+  
+  # tau is the learning rate
+  if(length(sigma)>1){ # if we have individual mutation step sizes 
+    tau = c(1/sqrt(2*n), 1/sqrt(2*sqrt(n))); # we have a global and coordinate wise learning rate
+  } else { # if we have a global mutation step size
+    tau = 1/sqrt(n); 
   }
   
+  ########################################################
+  
   # Evolution loop
-  Pnew = matrix(data=NA, nrow=mu, ncol=n); # Offspring/children population
+  
   while(evalcount<=(budget-lambda) && !target_hit(f, IOHproblem)){
     Prec = recombine(P, f, lambda, rho);
     Pmut = mutate(Prec, tau);
-    f = evaluate(Pmut,IOHproblem);
+    f = evaluate(Pmut,IOHproblem); # mu komma lambda selection?
     idxs = select(f,  mu);
     Pnew = Pmut[idxs];
     f = f[idxs];
@@ -99,6 +110,7 @@ evolutionary_strategy <- function(IOHproblem){
     }
   }
 }
+
 
 benchmark_algorithm(user_alg=evolutionary_strategy,
                     instances=seq(5),
